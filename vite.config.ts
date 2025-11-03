5645bfce5416c590ac632b996a8c4698e35735e0
roney-thomas-george <byroney3@gmail.com>
2025-11-03 22:13:04 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/swift-aid-hero/',
   server: {
     host: "::",
     port: 8080,
